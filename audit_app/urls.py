668ad0da904adde249e9b7afff62997aec314199
CWDamm-Kint <chris.damm@k-int.co.uk>
2026-05-15 10:17:56 +0100

    remove temporary audit view

--- audit_app/urls.py
+++ audit_app/urls.py
@@ -1,7 +1,10 @@
 from django.urls import re_path, path
-from django.http import HttpResponse
-from audit_app.views.audit import Audit
+from audit_app.views.api.edit_log import ResourceEditLogAPIView
 
 urlpatterns = [
-    path("audit/", Audit.as_view()),
+    path(
+        "api/audit/edit-log",
+        ResourceEditLogAPIView.as_view(),
+        name="audit-app-edit-log",
+    ),
 ]
