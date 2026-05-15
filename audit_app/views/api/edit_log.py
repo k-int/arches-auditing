1f0a8a8eabf5bec813972c60a50cc659f100b99f
CWDamm-Kint <chris.damm@k-int.co.uk>
2026-05-15 15:28:03 +0100

    fetch nodegroups once, outside of loop

--- audit_app/views/api/edit_log.py
+++ audit_app/views/api/edit_log.py
@@ -46,6 +46,11 @@ class ResourceEditLogAPIView(View):
 
         permitted_edits = []
 
+        nodegroup_ids = [uuid.UUID(edit.nodegroupid) for edit in filtered_edits if edit.nodegroupid]
+        nodegroups_by_id = models.NodeGroup.objects.filter(
+            pk__in=nodegroup_ids
+        ).in_bulk()
+
         for edit in filtered_edits:
 
             resource_instance = resource_lookup.get(edit.resourceinstanceid)
@@ -56,37 +61,32 @@ class ResourceEditLogAPIView(View):
 
             card_name = graph_card_map.get(str(nodegroup_id), _("Unknown Card"))
 
-            nodegroups_by_id = models.NodeGroup.objects.filter(
-                pk__in=[uuid.UUID(edit.nodegroupid) for edit in filtered_edits if edit.nodegroupid]
-            ).in_bulk()
+            if edit.nodegroupid:
+                nodegroup = nodegroups_by_id.get(uuid.UUID(edit.nodegroupid))
+                if nodegroup and not request.user.has_perm("read_nodegroup", nodegroup):
+                    continue
 
-            for edit in filtered_edits:
-                if edit.nodegroupid:
-                    nodegroup = nodegroups_by_id.get(uuid.UUID(edit.nodegroupid))
-                    if nodegroup and not request.user.has_perm("read_nodegroup", nodegroup):
-                        continue
-
-                    permitted_edits.append(
-                    {
-                        "editlogid": str(edit.editlogid),
-                        "resourceinstanceid": str(edit.resourceinstanceid),
-                        "resource_name": str(resource_name),
-                        "graph_name": graph_name,
-                        "transactionid": (
-                            str(edit.transactionid) if edit.transactionid else None
-                        ),
-                        "edittype": edit.edittype,
-                        "edittype_label": EDIT_TYPE_LABELS.get(edit.edittype, edit.edittype),
-                        "timestamp": edit.timestamp.isoformat() if edit.timestamp else None,
-                        "userid": edit.userid,
-                        "user_firstname": edit.user_firstname,
-                        "user_lastname": edit.user_lastname,
-                        "user_username": edit.user_username,
-                        "user_email": edit.user_email,
-                        "nodegroupid": edit.nodegroupid,
-                        "tileinstanceid": edit.tileinstanceid,
-                        "card_name": card_name,
-                        "note": edit.note,
-                    })
+                permitted_edits.append(
+                {
+                    "editlogid": str(edit.editlogid),
+                    "resourceinstanceid": str(edit.resourceinstanceid),
+                    "resource_name": str(resource_name),
+                    "graph_name": graph_name,
+                    "transactionid": (
+                        str(edit.transactionid) if edit.transactionid else None
+                    ),
+                    "edittype": edit.edittype,
+                    "edittype_label": EDIT_TYPE_LABELS.get(edit.edittype, edit.edittype),
+                    "timestamp": edit.timestamp.isoformat() if edit.timestamp else None,
+                    "userid": edit.userid,
+                    "user_firstname": edit.user_firstname,
+                    "user_lastname": edit.user_lastname,
+                    "user_username": edit.user_username,
+                    "user_email": edit.user_email,
+                    "nodegroupid": edit.nodegroupid,
+                    "tileinstanceid": edit.tileinstanceid,
+                    "card_name": card_name,
+                    "note": edit.note,
+                })
             
         return JSONResponse({"edits": permitted_edits})
