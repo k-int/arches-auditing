a839d763c93702e56e9be34fae16af83ccbd8341
CWDamm-Kint <chris.damm@k-int.co.uk>
2026-05-18 17:03:45 +0100

    remove incorrect import

--- audit_app/src/audit/AuditDashboard.vue
+++ audit_app/src/audit/AuditDashboard.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-    import { onSort, onMounted, ref } from "vue";
+    import { onMounted, ref } from "vue";
     import { useGettext } from "vue3-gettext";
     import { useToast } from "primevue/usetoast";
     import DataTable from 'primevue/datatable';
